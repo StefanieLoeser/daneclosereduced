ec13d63be66d371a12667d49824649705c984285
StefanieLoeser <stefanie.loeser@gmx.net>
2022-12-05 16:49:51 +0100

    add styled components and basic styling of landing page, render grid image overview

--- components/Post.js
+++ components/Post.js
@@ -1,36 +1,27 @@
 import Link from "next/link";
-import Image from "next/image";
+import styled from "styled-components";
 //to use Image with an external url, add some config on next.config.js
 //for more info, check out these docs https://nextjs.org/docs/basic-features/image-optimization
 
 export default function Post({ post, featuredMedia }) {
-  console.log("Featured Media", featuredMedia);
-
   return (
-    <div className="card mb-3" style={{ maxWidth: "540px" }}>
-      <div className="row g-0">
-        <div className="col-md-4">
-          <Link href={`/posts/${post.slug}`}>
-            <Image
-              src={featuredMedia["source_url"]}
-              width={180}
-              height={120}
-              alt={featuredMedia["alt_text"]}
-            />
-          </Link>
-        </div>
-        <div className="col-md-8">
-          <div className="card-body">
-            <h5 className="card-title">{post.title.rendered}</h5>
-            <div
-              className="card-text"
-              dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
-            ></div>
-
-            <Link href={`/posts/${post.slug}`}>See more</Link>
-          </div>
-        </div>
-      </div>
-    </div>
+    <LinkWrapper>
+      <Link href={`/posts/${post.slug}`}>
+        {featuredMedia && (
+          <FeatureImage
+            src={featuredMedia["source_url"]}
+            alt={featuredMedia["alt_text"]}
+          />
+        )}
+      </Link>
+    </LinkWrapper>
   );
 }
+
+const LinkWrapper = styled.div`
+  position: relative;
+`;
+
+const FeatureImage = styled.img`
+  width: 50%;
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,13 @@
 import Head from "next/head";
+import styled from "styled-components";
 
 import { getPosts, getMedia } from "../utils/wordpress";
 
 import Post from "../components/Post";
 
 export default function Home({ posts }) {
-  posts.map((post) => {
-    console.log("Embedded", post["_embedded"]["wp:featuredmedia"]);
-  });
-
   const jsxPosts = posts.map((post) => {
-    const featuredMedia = post["_embedded"]["wp:featuredmedia"][0];
-    console.log("featuredMedia Index", featuredMedia);
+    const featuredMedia = post["_embedded"]["wp:featuredmedia"]?.[0];
     return <Post post={post} featuredMedia={featuredMedia} key={post.id} />;
   });
 
@@ -19,21 +15,20 @@ export default function Home({ posts }) {
     <>
       <Head>
         <title>Dane Close Reduced</title>
-        <meta
-          name="description"
-          content="Keep up to date with the latest trends in tech"
-        />
+        <meta name="description" />
         <link rel="icon" href="/favicon.ico" />
-        {/* You can add more metadata here, like open graph tags for social media, etc */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Gloria+Hallelujah&family=Open+Sans:wght@300;400&family=Permanent+Marker&family=Rock+Salt&family=Share+Tech&display=swap"
+          rel="stylesheet"
+        />
       </Head>
-
-      <div className="container pt-5">
-        <h1 className="text-center pb-5">dance close reduced</h1>
-        <div className="row">
-          <div className="col-lg-8">{jsxPosts}</div>
-          <div className="col-lg-4"></div>
-        </div>
-      </div>
+      <BodyContainer>
+        <main>
+          <TitleDC>dane close</TitleDC>
+          <TitleRed>reduced</TitleRed>
+          <GridContainer>{jsxPosts}</GridContainer>
+        </main>
+      </BodyContainer>
     </>
   );
 }
@@ -50,3 +45,38 @@ export async function getStaticProps({ params }) {
     revalidate: 10, // In seconds
   };
 }
+
+const BodyContainer = styled.div`
+  background-color: #ff9fea;
+  padding: 3rem;
+  margin: 0;
+`;
+
+const TitleDC = styled.h1`
+  position: absolute;
+  z-index: 1;
+  left: 235px;
+  top: 160px;
+
+  font-family: "Gloria Hallelujah", sans-serif;
+  text-transform: uppercase;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 4.5rem;
+  line-height: 95px;
+
+  color: #000;
+
+  filter: blur(4px);
+`;
+
+const TitleRed = styled(TitleDC)`
+  left: 734px;
+  top: 414px;
+`;
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 20px;
+`;
