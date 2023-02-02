62862a5653368eefe197abd0beee6313318b69a3
Stefanie Loeser <stefanie.loeser@gmx.net>
2023-02-02 17:37:52 +0100

    Update Post.js

--- components/Post.js
+++ components/Post.js
@@ -1,23 +1,13 @@
 import Link from "next/link";
 import styled from "styled-components";
 import Image from "next/legacy/image";
-//to use Image with an external url, add some config on next.config.js
-//for more info, check out these docs https://nextjs.org/docs/basic-features/image-optimization
 
 export default function Post({ post, featuredMedia }) {
-  console.log(post);
+
   return (
     <Link href={`/posts/${post.slug}`} legacyBehavior>
       <ImageWrapper>
         {featuredMedia && (
-          //   <Image
-          //     src={
-          //       featuredMedia["media_details"]["sizes"]["medium"]["source_url"]
-          //     }
-          //     layout="responsive"
-          //     objectFit="contain"
-          //     alt={featuredMedia["alt_text"]}
-          //   />
           <FeatureImage
             src={
               featuredMedia["media_details"]?.["sizes"]?.["medium"]?.[
